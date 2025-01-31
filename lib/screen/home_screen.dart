88cce203b80db143dfa6324ee1ba4f68a497b81d
Rasel <aburaselahamed99@gmail.com>
2025-01-31 22:15:59 +0600

    card shape complete

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -11,31 +11,31 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff242C3B),
+      backgroundColor: const Color(0xff242C3B),
       appBar: AppBar(
-        backgroundColor: Color(0xff242C3B),
-        title: Text(
+        backgroundColor: const Color(0xff242C3B),
+        title: const Text(
           "Choose Your Bike",
           style: TextStyle(
               fontWeight: FontWeight.bold, fontSize: 20, color: Colors.white),
         ),
         actions: [
           Container(
-            margin: EdgeInsets.symmetric(horizontal: 16),
+            margin: const EdgeInsets.symmetric(horizontal: 16),
             height: 40,
             width: 50,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(
                 10,
               ),
-              gradient: LinearGradient(
+              gradient: const LinearGradient(
                 colors: [
                   Color(0xff34C8E8),
                   Color(0xff4E4AF2),
                 ],
               ),
             ),
-            child: Icon(
+            child: const Icon(
               Icons.search,
               color: Colors.white,
             ),
@@ -45,12 +45,8 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Stack(
         children: <Widget>[
           SizedBox(
-
             width: double.infinity,
-            height: MediaQuery
-                .of(context)
-                .size
-                .height,
+            height: MediaQuery.of(context).size.height,
           ),
           Positioned(
             bottom: 0,
@@ -65,10 +61,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         Color(0xff37B6E9),
                       ])),
                   width: double.infinity,
-                  height: MediaQuery
-                      .of(context)
-                      .size
-                      .height / 1.2),
+                  height: MediaQuery.of(context).size.height / 1.2),
             ),
           ),
 
@@ -80,34 +73,42 @@ class _HomeScreenState extends State<HomeScreen> {
                 height: 240, // Ensure height is enough
                 width: double.infinity, // Ensure full width
                 decoration: BoxDecoration(
-                  color: Colors.red
-               /*   gradient: LinearGradient(
+                  gradient: LinearGradient(
                     colors: [
-                      Color(0xff222834),
-
-                      Color(0xff353F54).withOpacity(.7),
+                      const Color(0xff222834),
+                      const Color(0xff353F54).withOpacity(.7),
                     ],
-                  ),*/
+                  ),
                 ),
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ClipPath(
-              clipper: CardClipper(),
-              child: Container(
-                color: Colors.green,
-                width: 200,
-                height: 240,
-              ),
+          GridView.builder(
+
+            padding: const EdgeInsets.all(16),
+            itemCount: 10,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: .6,
+              crossAxisSpacing: 5.0,
+              mainAxisSpacing: 10, // Adjust this value to decrease the bottom space
             ),
+            itemBuilder: (BuildContext context, int index) {
+              return ClipPath(
+                clipper: CardClipper(),
+                child: Container(
+                  color: Colors.green,
+
+                ),
+              );
+            },
           ),
         ],
       ),
     );
   }
 }
+
 class TopContainerClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
@@ -115,15 +116,14 @@ class TopContainerClipper extends CustomClipper<Path> {
     path.moveTo(20, 0);
     path.quadraticBezierTo(0, 0, 0, 20);
 
-    path.lineTo(0, size.height-20);
-    path.quadraticBezierTo(0, size.height,20 , size.height);
+    path.lineTo(0, size.height - 20);
+    path.quadraticBezierTo(0, size.height, 20, size.height);
 
-    path.lineTo(size.width-15, size.height-50);
-    path.quadraticBezierTo(size.width, size.height-50,size.width , size.height-65);
-
+    path.lineTo(size.width - 15, size.height - 50);
+    path.quadraticBezierTo(size.width, size.height - 50, size.width, size.height - 65);
 
     path.lineTo(size.width, 20);
-    path.quadraticBezierTo(size.width, 0,size.width-20 , 0);
+    path.quadraticBezierTo(size.width, 0, size.width - 20, 0);
 
     path.close();
     return path;
@@ -134,22 +134,22 @@ class TopContainerClipper extends CustomClipper<Path> {
     return true;
   }
 }
+
 class CardClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
-    path.moveTo(20, 0);
-    path.quadraticBezierTo(0, 0, 0, 20);
+    path.moveTo(10, 40);
+    path.quadraticBezierTo(0, 40, 0, 70);
 
-    path.lineTo(0, size.height-20);
-    path.quadraticBezierTo(0, size.height,20 , size.height);
+    path.lineTo(0, size.height - 20);
+    path.quadraticBezierTo(0, size.height, 20, size.height);
 
-    path.lineTo(size.width-15, size.height-50);
-    path.quadraticBezierTo(size.width, size.height-55,size.width , size.height-65);
-
+    path.lineTo(size.width - 10, size.height - 40);
+    path.quadraticBezierTo(size.width, size.height - 40, size.width, size.height - 60);
 
     path.lineTo(size.width, 20);
-    path.quadraticBezierTo(size.width, 0,size.width-20 , 0);
+    path.quadraticBezierTo(size.width, 0, size.width - 20, 0);
 
     path.close();
     return path;
@@ -160,11 +160,6 @@ class CardClipper extends CustomClipper<Path> {
     return true;
   }
 }
-
-
-
-
-
 
 class BottomContainerClipper extends CustomClipper<Path> {
   @override
